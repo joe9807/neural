103562943d9ee0875be7a3cad9225f8bc89f0988
joe9807 <joe9807@gmail.com>
2022-10-15 17:23:28 +0300

    swagger added

--- src/main/java/application/controllers/NeuralController.java
+++ src/main/java/application/controllers/NeuralController.java
@@ -1,6 +1,7 @@
 package application.controllers;
 
 import application.ui.NeuralUI;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,10 @@ public class NeuralController {
     @Autowired
     private NeuralUI neuralUI;
 
+    @Operation(summary = "Запустить Тестовый Neural UI")
     @GetMapping("ui")
     public String showUI(){
         new Thread(() -> neuralUI.init()).start();
-        return "UI";
+        return "Neural UI has started in separated window.";
     }
 }
