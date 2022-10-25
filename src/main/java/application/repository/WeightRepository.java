875a73af41e41de6e146ec720a714fe5f4c864c1
joe9807 <joe9807@gmail.com>
2022-10-25 13:00:28 +0300

    repository improvement

--- src/main/java/application/repository/WeightRepository.java
+++ src/main/java/application/repository/WeightRepository.java
@@ -1,15 +1,12 @@
 package application.repository;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import application.repository.entity.Weight;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface WeightRepository extends CrudRepository<Weight, Integer> {
-    @Query("from Weight w where w.level=:level order by id")
-    List<Weight> findAllByLevel(@Param("level") int level);
+    List<Weight> findAllByLevel(int level);
 }
