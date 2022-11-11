14c54dff7c1f2533c9c2a91ac0f5225fdbc81932
joe9807 <joe9807@gmail.com>
2022-11-11 19:20:57 +0300

    noise parameter introduced

--- src/main/java/application/controllers/NeuralController.java
+++ src/main/java/application/controllers/NeuralController.java
@@ -2,6 +2,7 @@ package application.controllers;
 
 import application.ui.NeuralDialog;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class NeuralController {
     private NeuralDialog neuralDialog;
 
     @Operation(summary = "Запустить Тестовый Neural UI")
+    @Parameter(name = "noise", example = "0.01")
     @GetMapping("ui")
     public String showUI(double noise){
         new Thread(() -> neuralDialog.init(noise)).start();
