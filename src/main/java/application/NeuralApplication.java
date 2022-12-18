4e9cc8b3a1cd0ec802df22a6020093855ea7c58b
joe9807 <joe9807@gmail.com>
2022-12-18 22:04:14 +0300

    test comment

--- src/main/java/application/NeuralApplication.java
+++ src/main/java/application/NeuralApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NeuralApplication {
     public static void main(String args[]){
         SpringApplication.run(NeuralApplication.class, args);
+        //test comment
     }
 }
